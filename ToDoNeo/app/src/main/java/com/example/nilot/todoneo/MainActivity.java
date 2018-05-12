5dfe282f10905d50d67dae1f8b846a5ee7a3cb72
Nilotpal Pramanik <33095476+goodday451999@users.noreply.github.com>
2018-05-12 20:26:05 +0530

    Initial Commit

---
+++ ToDoNeo/app/src/main/java/com/example/nilot/todoneo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.nilot.todoneo;
+
+import android.app.Activity;
+import android.support.v7.appcompat.*;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
